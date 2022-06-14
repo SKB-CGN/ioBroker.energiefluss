5c56d11d05f0a2dfb2fd5e276ca6b27d02141183
SKB-CGN <58603329+SKB-CGN@users.noreply.github.com>
2022-06-14 17:03:12 +0200

    Delete node_modules directory

--- node_modules/@iobroker/adapter-dev/build/util.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function escapeRegExp(value: string): string;
-export declare function padRight(text: string, totalLength: number): string;
-export declare function error(message: string): void;
-export declare function die(message: string): never;
-export declare function interceptErrors(func: () => Promise<void>): () => Promise<void>;
